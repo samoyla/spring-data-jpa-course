ff6ac5d991150f85145b80fcba7cf6c00fbde43e
samoyla <samoyla@hotmail.com>
2023-11-10 16:43:46 +0100

    student class and student repository

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,11 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args ->{
+            Student maria = new Student("Maria", "Jones", "maria.jones@mail.com", 21);
+            studentRepository.save(maria);
+        };
+    }
 }
